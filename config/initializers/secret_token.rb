e8bc39a0fc5e0736223784527858a779804abfe5
Ben Sharaf <bsharaf@bates.edu>
2014-05-21 15:51:44 -0400

    Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weathervein::Application.config.secret_key_base = '4f0af3ebddfe7898320fe24326482c502c3504b29f2deca8e42067b0c2affad5adff434c40ad51367c89b5845a6a226e82e0c8bb0c4f1e0ad3686ad906c72db2'
